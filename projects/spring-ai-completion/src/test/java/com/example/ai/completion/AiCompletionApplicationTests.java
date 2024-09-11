cf335909ba9446702a13c6ac18c934d5d3045c1d
ericm48 <me_manley@yahoo.com>
2024-09-11 16:58:02 -0600

    -Initial Commit.

---
+++ projects/spring-ai-completion/src/test/java/com/example/ai/completion/AiCompletionApplicationTests.java
@@ -0,0 +1,38 @@
+package com.example.ai.completion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class AiCompletionApplicationTests {
+
+	@Autowired
+	private ChatClient.Builder builder;
+
+	@Test
+	void completionTest() {
+
+		ChatClient chatClient = null;
+		String question = "When was Microsoft founded?";
+		String answer = null;
+		String realAnswer = "1975";
+
+		chatClient = builder.build();
+
+		assertTrue(chatClient != null);
+
+		answer = chatClient
+				.prompt()
+				.user(question)
+				.call()
+				.content();
+
+		assertTrue(answer.contains(realAnswer));
+
+	}
+
+}
